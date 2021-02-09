ef85634855923a557d72152caf07bc79eb006634
Sandro Scalco <sandro.scalco@liitu.ch>
2021-02-09 21:07:32 +0100

    update info

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const OpenTimestamps = require('opentimestamps');
 
 const file = Buffer.from('5468652074696d657374616d70206f6e20746869732066696c6520697320696e636f6d706c6574652c20616e642063616e2062652075706772616465642e0a','hex');
 const detached = OpenTimestamps.DetachedTimestampFile.fromBytes(new OpenTimestamps.Ops.OpSHA256(), file);
+console.log("detached: " + detached);
 OpenTimestamps.stamp(detached).then( ()=>{
   const fileOts = detached.serializeToBytes();
 
   console.log("verify");
   const detachedOts = OpenTimestamps.DetachedTimestampFile.deserialize(fileOts);
+  console.log("detachedOts: " + detachedOts);
   let options = {};
   // options.ignoreBitcoinNode - Ignore verification with bitcoin node 
   options.ignoreBitcoinNode = true;
   // options.timeout - Adjust the request timeout (default: 1000) 
   options.timeout = 5000;
-  OpenTimestamps.verify(detachedOts,detached,options).then(verifyResult => {
+  /*OpenTimestamps.verify(detachedOts,detached,options).then(verifyResult => {
     // return an object containing timestamp and height for every attestation if verified, undefined otherwise.
     console.log(verifyResult);
     // prints:
     // { bitcoin: { timestamp: 1521545768, height: 514371 },
     //   litecoin: { timestamp: 1521540398, height: 1388467 } }
   
-  });
+  });*/
 
 });
 
 const infoResult = OpenTimestamps.info(detached);
 console.log(infoResult);
+
